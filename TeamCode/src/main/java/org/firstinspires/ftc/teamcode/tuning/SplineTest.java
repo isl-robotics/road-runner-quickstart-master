bc7bfb0ec5d1366874e0fd89ce777b1ba3db340f
Erwan <erwan.ducuing@hotmail.com>
2026-02-26 11:29:17 +0100

    Fixed offset slightly and made auto with splines

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tuning/SplineTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tuning/SplineTest.java
@@ -11,7 +11,7 @@ import org.firstinspires.ftc.teamcode.TankDrive;
 public final class SplineTest extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
-        Pose2d beginPose = new Pose2d(57,  -14.5, Math.PI);
+        Pose2d beginPose = new Pose2d(61,  -14.5, Math.PI);
         if (TuningOpModes.DRIVE_CLASS.equals(MecanumDrive.class)) {
             MecanumDrive drive = new MecanumDrive(hardwareMap, beginPose);
 
@@ -22,13 +22,49 @@ public final class SplineTest extends LinearOpMode {
 
                         //.splineTo(new Vector2d(30, 30), Math.PI / 2)
                         //.splineTo(new Vector2d(0, 60), Math.PI)
-                        .splineToLinearHeading(new Pose2d(0, 0, 0), Math.PI)
-                        .waitSeconds(10)
-                        .splineToLinearHeading(new Pose2d(57,-14.5,Math.PI), 0)
+                        //.splineToLinearHeading(new Pose2d(0, 0, 0), Math.PI)
+                        //.waitSeconds(10)
+                        /*
+                        .strafeToLinearHeading(new Vector2d(50, -14.5), -Math.toRadians(160))
+                        .waitSeconds(1)
+                        .strafeToLinearHeading(new Vector2d(mmToIn(900), -14.5), -Math.toRadians(90))
+                        //.waitSeconds(0)
+                        .lineToY(-50)
+                        .waitSeconds(0)
+                        .strafeToLinearHeading(new Vector2d(50, -14.5), -Math.toRadians(160))
+                        .waitSeconds(1)
+                        .strafeToLinearHeading(new Vector2d(mmToIn(300), -14.5), -Math.toRadians(90))
+                        .waitSeconds(0)
+                        .lineToY(-50)
+                        .waitSeconds(0)
+                        .strafeToLinearHeading(new Vector2d(50, -14.5), -Math.toRadians(160))
+                        .waitSeconds(1)
+                        .splineToLinearHeading(new Pose2d(61,-14.5,Math.PI), 0)
+
+                         */
+                        .splineToLinearHeading(new Pose2d(50, -14.5, -Math.toRadians(160)), -Math.toRadians(160))
+                        .waitSeconds(1)
+                        .splineToLinearHeading(new Pose2d(mmToIn(900), -35,-Math.toRadians(90)), -Math.toRadians(90))
+                        .splineToLinearHeading(new Pose2d(mmToIn(900), -50,-Math.toRadians(90)), -Math.toRadians(90))
+                        .waitSeconds(1)
+                        .splineToLinearHeading(new Pose2d(50, -14.5, -Math.toRadians(160)), Math.toRadians(20))
+                        .waitSeconds(1)
+                        .splineToLinearHeading(new Pose2d(mmToIn(300), -35,-Math.toRadians(90)), -Math.toRadians(90))
+                        .splineToLinearHeading(new Pose2d(mmToIn(300), -50,-Math.toRadians(90)), -Math.toRadians(90))
+                        .waitSeconds(1)
+                        .splineToLinearHeading(new Pose2d(50, -14.5, -Math.toRadians(160)), Math.toRadians(20))
+                        .waitSeconds(1)
+                        .strafeToLinearHeading(new Vector2d(40,-14.5), Math.PI)
+                        .waitSeconds(5)
+                        .strafeToLinearHeading(new Vector2d(61,-14.5), Math.PI)
                         .build());
         }
         else {
             throw new RuntimeException();
         }
+    }
+
+    public double mmToIn (double mm){
+        return mm*0.03937;
     }
 }
