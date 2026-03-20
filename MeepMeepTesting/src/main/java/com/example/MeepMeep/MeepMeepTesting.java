45d7597231a69c86535a7440a88544d93e871db2
Siddharth <siddharth.r.bist@gmail.com>
2026-03-20 23:09:05 +0100

    FIXED MEEPMEEP

--- MeepMeepTesting/src/main/java/com/example/MeepMeep/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/MeepMeep/MeepMeepTesting.java
@@ -7,30 +7,40 @@ import org.rowlandhall.meepmeep.MeepMeep;
 import org.rowlandhall.meepmeep.roadrunner.DefaultBotBuilder;
 import org.rowlandhall.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
 
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
 public class MeepMeepTesting {
-    public static void main(String[] args) {
-        MeepMeep meepMeep = new MeepMeep(800);
+    public static void main(String[] args) throws IOException {
+        MeepMeep meepMeep = new MeepMeep(780);
+        Pose2d beginPose = new Pose2d(-61,  -14.5, Math.PI);
 
         RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
                 .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
-                .followTrajectorySequence(drive -> drive.trajectorySequenceBuilder(new Pose2d(0, 0, 0))
-                        .splineToLinearHeading(new Pose2d(50, -14.5, -Math.toRadians(160)), -Math.toRadians(160))
-                        .waitSeconds(1)
-                        .splineToLinearHeading(new Pose2d(mmToIn(900), -35,-Math.toRadians(90)), -Math.toRadians(90))
-                        .splineToLinearHeading(new Pose2d(mmToIn(900), -50,-Math.toRadians(90)), -Math.toRadians(90))
-                        .waitSeconds(1)
-                        .splineToLinearHeading(new Pose2d(50, -14.5, -Math.toRadians(160)), Math.toRadians(20))
-                        .waitSeconds(1)
-                        .splineToLinearHeading(new Pose2d(mmToIn(300), -35,-Math.toRadians(90)), -Math.toRadians(90))
-                        .splineToLinearHeading(new Pose2d(mmToIn(300), -50,-Math.toRadians(90)), -Math.toRadians(90))
-                        .waitSeconds(1)
-                        .splineToLinearHeading(new Pose2d(50, -14.5, -Math.toRadians(160)), Math.toRadians(20))
-                        .waitSeconds(1)
+                .followTrajectorySequence(drive -> drive.trajectorySequenceBuilder(beginPose)
+                        .strafeTo(new Vector2d(mmToIn(-300), -20))
+                        .waitSeconds(0.5)
+                        .strafeTo(new Vector2d(mmToIn(-300), -30))
+                        .strafeTo(new Vector2d(mmToIn(-300),-56))
+                        .strafeTo(new Vector2d(mmToIn(-300), -20))
+                        .waitSeconds(0.5)
+                        .strafeTo(new Vector2d(mmToIn(300), -30))
+                        .strafeTo(new Vector2d(mmToIn(300),-55))
+                        .strafeTo(new Vector2d(mmToIn(-300), -20))
+                        .waitSeconds(0.5)
+                        .strafeTo(new Vector2d(mmToIn(860), -20))
+                        .strafeTo(new Vector2d(mmToIn(860), -50))
+                        .strafeTo(new Vector2d(mmToIn(-300), -20))
+                        .waitSeconds(0.3)
+                        .strafeTo(new Vector2d(mmToIn(-200),-38))
                         .build());
 
-
-        meepMeep.setBackground(MeepMeep.Background.GRID_GRAY)
+        BufferedImage decodeField = ImageIO.read(new File("C:\\Users\\DELL\\Desktop\\FTC-202526\\Robot_Controller_Builds\\road-runner-quickstart-master\\MeepMeepTesting\\src\\main\\java\\com\\example\\MeepMeep\\decode-field.jpg"));
+        meepMeep.setBackground(decodeField)
                 .setDarkMode(true)
                 .setBackgroundAlpha(0.95f)
                 .addEntity(myBot)
