5cefcfc6aeab5a66a4b9909f2527896ecc81174a
Gwornac <erwan.ducuing@hotmail.com>
2026-03-20 12:38:05 +0100

    Red Auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
@@ -483,7 +483,22 @@ public final class MecanumDrive {
                 ),
                 beginPose, 0.0,
                 defaultTurnConstraints,
-                defaultVelConstraint, defaultAccelConstraint
-        );
+                defaultVelConstraint, defaultAccelConstraint);
+    }
+
+    public TrajectoryActionBuilder actionBuilder(Pose2d beginPose, boolean red) {
+        return new TrajectoryActionBuilder(
+                TurnAction::new,
+                FollowTrajectoryAction::new,
+                new TrajectoryBuilderParams(
+                        1e-6,
+                        new ProfileParams(
+                                0.25, 0.1, 1e-2
+                        )
+                ),
+                beginPose, 0.0,
+                defaultTurnConstraints,
+                defaultVelConstraint, defaultAccelConstraint, pose -> new Pose2dDual<>(
+                pose.position.x, pose.position.y.unaryMinus(), pose.heading.inverse()));
     }
 }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RedAuto.java
@@ -0,0 +1,110 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.SequentialAction;
+import com.acmerobotics.roadrunner.TrajectoryActionBuilder;
+import com.acmerobotics.roadrunner.TranslationalVelConstraint;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.acmerobotics.roadrunner.ftc.Actions;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+
+@Autonomous
+public class RedAuto extends AutoInit{
+    private TrajectoryActionBuilder traj1;
+    private TrajectoryActionBuilder traj2;
+    private TrajectoryActionBuilder traj3;
+    private TrajectoryActionBuilder traj4;
+    private TrajectoryActionBuilder traj5;
+    private TrajectoryActionBuilder traj6;
+    private TrajectoryActionBuilder traj7;
+    private TrajectoryActionBuilder traj8;
+    private TrajectoryActionBuilder traj9;
+
+    @Override
+    public void extraInit(){
+        Pose2d beginPose = new Pose2d(61,  14.5, Math.PI);
+        MecanumDrive drive = new MecanumDrive(hardwareMap, beginPose);
+
+
+        traj1 = drive.actionBuilder(beginPose, true)
+                .strafeToLinearHeading(new Vector2d(50, -15), -Math.toRadians(160))
+                .waitSeconds(1);
+
+        traj2 = traj1.endTrajectory().fresh()
+                .splineToLinearHeading(new Pose2d(mmToIn(900), -38,-Math.toRadians(90)), -Math.toRadians(90))
+              //  .splineToLinearHeading(new Pose2d(mmToIn(900), -50,-Math.toRadians(90)), -Math.toRadians(90), new TranslationalVelConstraint(25))
+
+        //  .waitSeconds(0.3)
+                ;
+
+        traj3 = traj2.endTrajectory().fresh()
+                .strafeToConstantHeading(new Vector2d(mmToIn(900),-50),new TranslationalVelConstraint(25))
+                .waitSeconds(0.2);
+
+        traj4 = traj3.endTrajectory().fresh()
+                .strafeToLinearHeading(new Vector2d(50, -15), -Math.toRadians(160))
+                .waitSeconds(0.3);
+
+        traj5 = traj4.endTrajectory().fresh()
+                //.strafeToLinearHeading(new Vector2d(40, -20), -Math.toRadians(160))
+                .splineToLinearHeading(new Pose2d(mmToIn(300), -38,-Math.toRadians(90)), -Math.toRadians(90))
+                //.waitSeconds(1)
+                ;
+
+        traj6 = traj5.endTrajectory().fresh()
+                .strafeToConstantHeading(new Vector2d(mmToIn(300),-55),new TranslationalVelConstraint(25))
+                .waitSeconds(0.2);
+
+        traj7 = traj6.endTrajectory().fresh()
+                .strafeToLinearHeading(new Vector2d(50, -15), -Math.toRadians(160))
+                .waitSeconds(0.3);
+
+        traj8 = traj7.endTrajectory().fresh()
+                .strafeTo(new Vector2d(30, -20))
+                .waitSeconds(5);
+
+        traj9 = traj8.endTrajectory().fresh()
+                .strafeToLinearHeading(new Vector2d(61, -14.5), Math.PI)
+                .waitSeconds(1);
+    }
+    @Override
+    protected void runStrategy() {
+        Actions.runBlocking(
+                new SequentialAction(
+                        prepLauncher(),
+                        traj1.build(),
+                        startLauncher(),
+                        openGate(),
+                        launch(),
+                       // stopLauncher(),
+                    //    stopIntake(),
+                        closeGate(),
+                        traj2.build(),
+                   //     intake(),
+                        traj3.build(),
+                    //    stopIntake(),
+                        traj4.build(),
+                        startLauncher(),
+                        openGate(),
+                        launch(),
+                    //    stopLauncher(),
+                    //    stopIntake(),
+                        closeGate(),
+                        traj5.build(),
+                        intake(),
+                        traj6.build(),
+                   //     stopIntake(),
+                        traj7.build(),
+                        startLauncher(),
+                        openGate(),
+                        launch(),
+                        stopLauncher(),
+                        stopIntake(),
+                        closeGate(),
+                        traj8.build(),
+                        traj9.build()
+                )
+        );
+    }
+}
