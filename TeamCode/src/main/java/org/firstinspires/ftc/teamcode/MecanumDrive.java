0eb8c23bceae86b1d83b0a2be915f9e7c021b35b
Siddharth <siddharth.r.bist@gmail.com>
2026-04-12 15:57:33 +0200

    need to mirror the heading too
    for red alliance

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
@@ -240,7 +240,7 @@ public final class MecanumDrive {
         voltageSensor = hardwareMap.voltageSensor.iterator().next();
 
         if(Team.get() == Team.RED){
-            pose = new Pose2d(pose.component1().x,-pose.component1().y,pose.component2().toDouble());
+            pose = new Pose2d(pose.component1().x,-pose.component1().y,pose.component2().toDouble()); //TODO: MIRROR HEADING TOO
         }
 
         localizer = new PinpointLocalizer(hardwareMap, PARAMS.inPerTick, pose);
