f885cd9a33b0ca61c91212accb0bed7c05f0ea78
aehkos <alvinjeong08@gmail.com>
2026-06-11 21:47:25 +0200

    changed flywheel pid

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/LauncherController.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/LauncherController.java
@@ -24,8 +24,8 @@ public class LauncherController {
         launcherMotor1.setDirection(DcMotorEx.Direction.REVERSE);
         launcherMotor2.setDirection(DcMotorEx.Direction.REVERSE);
 
-        launcherMotor1.setPIDFCoefficients(DcMotorEx.RunMode.RUN_USING_ENCODER, new PIDFCoefficients(250,0.3,0.5,6.5));
-        launcherMotor2.setPIDFCoefficients(DcMotorEx.RunMode.RUN_USING_ENCODER, new PIDFCoefficients(250,0.3,0.5,6.5));
+        launcherMotor1.setPIDFCoefficients(DcMotorEx.RunMode.RUN_USING_ENCODER, new PIDFCoefficients(350,3.5,0.5,6));
+        launcherMotor2.setPIDFCoefficients(DcMotorEx.RunMode.RUN_USING_ENCODER, new PIDFCoefficients(350,3.5,0.5,6));
     }
 
     public void setVelocity(double angularRate){
